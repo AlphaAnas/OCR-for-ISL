426853da57a2c5356ca15971c4194655b11f6b3f
AlphaAnas <96944434+AlphaAnas@users.noreply.github.com>
2025-10-21 14:46:19 +0500

    Update app.py

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ interface OCRResult {
 
 
 
-const BACKEND_URL = 'https://dd7d700d4ca4.ngrok-free.app/parse-invoice-image';
+const BACKEND_URL = 'https://f99da2a748cc.ngrok-free.app/parse-invoice-image';
 
 function App() {
   const [file, setFile] = useState<File | null>(null);
